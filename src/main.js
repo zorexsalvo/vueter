5f6b4e4167ade1deece300c47980a3b8ab67e20f
Zorex Salvo <zorexsalvo@gmail.com>
2018-03-30 23:04:58 +0800

    Add firebase module

--- src/main.js
+++ src/main.js
@@ -1,10 +1,22 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import firebase from 'firebase';
 import App from './App';
 import router from './router';
 
 Vue.config.productionTip = false;
+
+const config = {
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+};
+
+firebase.initializeApp(config);
 
 /* eslint-disable no-new */
 new Vue({
