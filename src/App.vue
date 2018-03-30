e5fc4fb99fb192a7a87bb5a7d7e51427eb621443
Zorex Salvo <zorexsalvo@gmail.com>
2018-03-30 22:18:19 +0800

    Initial commit
    * vue init webpack vueter
    
    Create a project for my firebase application sample using
    vue.js and firebase.

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app" class="has-text-centered">
+    <img src="./assets/logo.png">
+    <router-view class="container"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style>
+  @import '../node_modules/bulma/css/bulma.css';
+</style>
