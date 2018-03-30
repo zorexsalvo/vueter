deccaf68c79915f34582f618af82add2fcb9aee9
Zorex Salvo <zorexsalvo@gmail.com>
2018-03-31 00:34:19 +0800

    Create a Home component added logout button

---
+++ src/components/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div class="columns">
+      <div class="column is-one-third is-offset-one-third">
+        You must be authenticated to access this page.
+      </div>
+    </div>
+    <div class="columns">
+      <div class="column is-one-third is-offset-one-third">
+        <div class="control">
+          <button
+            class="button is-fullwidth"
+            @click="signOut">Logout</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+import firebase from 'firebase';
+
+export default {
+  name: 'home',
+  methods: {
+    signOut() {
+      firebase.auth().signOut().then(() => {
+        this.$router.replace('login');
+      });
+    },
+  },
+};
+</script>
