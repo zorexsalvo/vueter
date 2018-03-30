d17921dbdd868aef7588a7d27ec410fcc82f9696
Zorex Salvo <zorexsalvo@gmail.com>
2018-03-30 22:18:37 +0800

    Create initial login page

---
+++ src/components/Login.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div class="columns">
+      <div class="column is-one-third is-offset-one-third">
+        <h6 class="title">Sign In</h6>
+
+        <div class="field">
+          <div class="control">
+            <input class="input" type="text" placeholder="Email">
+          </div>
+        </div>
+
+        <div class="field">
+          <div class="control">
+            <input class="input" type="password" placeholder="Password">
+          </div>
+        </div>
+
+        <div class="field is-grouped">
+          <div class="control">
+            <button class="button is-link">Login</button>
+          </div>
+          <div class="control">
+            <button class="button">Sign Up</button>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+  </div>
+</template>
